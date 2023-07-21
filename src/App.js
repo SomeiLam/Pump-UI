e1c8137ecdf772604a29ab0a575b062529d2a544
So Mei Amy Lam <lamsomeiamy@gmail.com>
2023-07-21 16:36:13 -0700

    handle disconnect

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import './App.css';
 import ControlButtons from './components/ControlButtons';
 import SpeedButtons from './components/SpeedButtons';
 import { PUMP_TYPE } from './utils/constants';
+import active from './assets/active.svg';
+import inactive from './assets/inactive.svg';
 
 const getSpeed = str => {
   const splited = str.split(':');
   const speed = splited[splited.length - 1];
   return speed;
+};
+
+const getTimeStamp = str => {
+  const splited = str.split(':');
+  const time = splited[splited.length - 2];
+  return time;
 };
 
 const Pump = ({
@@ -23,12 +31,17 @@ const Pump = ({
   handleDecreaseSpeed,
   handleStop,
   handleSet,
-  handleToggle
+  handleToggle,
+  isConnected
 }) => {
-  console.log(pumpType, speedValue, typeof speedValue)
   return (
     <section className={`pump-container ${pumpType === PUMP_TYPE.WATER_PUMP ? 'water' : 'oil'
       }`}>
+      {!isConnected && <div className={`pump-disconnected
+      ${pumpType === PUMP_TYPE.WATER_PUMP ? 'water-d' : 'oil-d'}`}>
+        <h1>{pumpType === PUMP_TYPE.WATER_PUMP ?
+          'Water ' : 'Oil '}Pump is Disconnected</h1>
+      </div>}
       <section className='pump-control'>
         <div className='pump-animation'>
           <h1>{pumpType === PUMP_TYPE.WATER_PUMP ?
@@ -68,6 +81,11 @@ const Pump = ({
 const App = () => {
   const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+  const [isReconnecting, setIsReconnecting] = useState(false);
+  const [waterTimeStamp, setWaterTimeStamp] = useState('');
+  const [oilTimeStamp, setOilTimeStamp] = useState('');
+  const [isWaterConnected, setIsWaterConnected] = useState(false);
+  const [isOilConnected, setIsOilConnected] = useState(false);
   const [waterTemp, setWaterTemp] = useState(0);
   const [waterSpeed, setWaterSpeed] = useState(0);
   const [showWaterTemp, setShowWaterTemp] = useState(false);
@@ -166,6 +184,14 @@ const App = () => {
     };
   };
 
+  const reconnect = () => {
+    setIsReconnecting(true);
+    setTimeout(() => {
+      setIsReconnecting(false);
+      window.location.reload();
+    }, 1000);
+  };
+
   useEffect(() => {
     const pumpSocket = new WebSocket('ws://192.168.10.1:9001');
     setSocket(pumpSocket);
@@ -176,11 +202,20 @@ const App = () => {
     };
 
     pumpSocket.onmessage = e => {
+      console.log('receiving data: ', e.data)
       const speed = getSpeed(e.data);
       if (e.data.slice(0, 10) === PUMP_TYPE.WATER_PUMP) {
         setWaterSpeed(speed);
+        setWaterTimeStamp(getTimeStamp(e.data));
+        if (!isWaterConnected) {
+          setIsWaterConnected(true);
+        };
       } else if (e.data.slice(0, 8) === PUMP_TYPE.OIL_PUMP) {
         setOilSpeed(speed);
+        setOilTimeStamp(getTimeStamp(e.data));
+        if (!isOilConnected) {
+          setIsOilConnected(true);
+        };
       };
     };
 
@@ -202,7 +237,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className={`App ${!isConnected ? 'disconnected' : ''}`}>
+    <div className='App'>
       <Pump
         pumpType={PUMP_TYPE.WATER_PUMP}
         speedValue={waterSpeed}
@@ -213,7 +248,8 @@ const App = () => {
         handleDecreaseSpeed={handleDecreaseSpeed}
         handleStop={handleStop}
         handleSet={handleSet}
-        handleToggle={handleToggle} />
+        handleToggle={handleToggle}
+        isConnected={isWaterConnected} />
       <Pump
         pumpType={PUMP_TYPE.OIL_PUMP}
         speedValue={oilSpeed}
@@ -224,7 +260,19 @@ const App = () => {
         handleDecreaseSpeed={handleDecreaseSpeed}
         handleStop={handleStop}
         handleSet={handleSet}
-        handleToggle={handleToggle} />
+        handleToggle={handleToggle}
+        isConnected={isOilConnected} />
+      {
+        !isConnected && <div className='disconnected'>
+          <h2>Connection Lost</h2>
+          <button
+            className={`reconnect ${isReconnecting ? 'reconnecting' : ''}`}
+            onClick={reconnect}
+            disabled={isReconnecting}>
+            {isReconnecting ? 'Reconnecting...' : 'Reconnect'}
+          </button>
+        </div>
+      }
     </div>
   );
 };
